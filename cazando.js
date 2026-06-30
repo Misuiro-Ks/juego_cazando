566ccc9367dafff90312e95b9bb0986176c9ab99
unknown <fabian.ni.fr@gmail.com>
2026-06-29 23:54:10 -0500

    agregar graficarGato

---
+++ cazando.js
@@ -0,0 +1,13 @@
+let canvas=document.getElementById("areaJuego");
+let ctx=canvas.getContext("2d");
+
+function graficarGato(){
+    const ancho = 70;
+    const alto = 30;
+    const x = (canvas.width - ancho) / 2;
+    const y = (canvas.height - alto) / 2;
+
+    ctx.fillStyle = "#e61c1c";
+    ctx.fillRect(x, y, ancho, alto);
+    
+}
